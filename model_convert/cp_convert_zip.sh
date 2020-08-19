000c9b47ed5dad50e670c1a6cd9384c2a22f6760
Dustin <dzubke@outlook.com>
2020-08-19 13:20:53 -0500

    sync ph5

--- model_convert/cp_convert_zip.sh
+++ model_convert/cp_convert_zip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # this command should be called from inside the onnx_coreml/ directory
-# command structure: bash cp_convert_zip.sh <base_path_to_model> <model_name> <num_frames>
+# command structure: bash cp_convert_zip.sh <base_path_to_model> <model_name> <num_frames> 
 
 # assigning the arguments
 model_path=$1
@@ -10,17 +10,17 @@ num_frames=$3
 
 # creating the functions
 copy_files(){
-    cp $1/best_model ./torch_models/$2_model.pth
+    cp $1/best_model.pth ./torch_models/$2_model.pth
     cp $1/best_preproc.pyc ./preproc/$2_preproc.pyc
     cp $1/*.yaml ./config/$2_config.yaml
 }
 
 convert_model(){
-    sed -i '' 's/import functions\.ctc/#import functions\.ctc/g' ../speech/models/ctc_model_train.py
-    python torch_to_onnx.py $1 --num_frames $2 --use_state_dict --half-precision 
+    sed -i 's/import functions\.ctc/#import functions\.ctc/g' ../speech/models/ctc_model_train.py
+    python torch_to_onnx.py --model-name $1 --num-frames $2 --use-state-dict --half-precision 
     python onnx_to_coreml.py $1
-    python validation.py $1 --num_frames $2
-    sed -i '' 's/#import functions\.ctc/import functions\.ctc/g' ../speech/models/ctc_model_train.py
+    python validation.py $1 --num-frames $2
+    sed -i 's/#import functions\.ctc/import functions\.ctc/g' ../speech/models/ctc_model_train.py
 }
 
 zip_files(){
