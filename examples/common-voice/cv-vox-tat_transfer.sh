bf2d0c774d1162e40bf0609aa0e96ef82c4bca93
Dustin <dzubke@outlook.com>
2021-01-27 20:53:42 +0000

    cleaned up dir on ph3, moved ./examples/ to ./data/

--- examples/common-voice/cv-vox-tat_transfer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# this command should be run within a VM that already has common-voice, voxforge,
-# and tatoeba processed data already installed. 
-# command structure: bash cv-vox-tat_transfer.sh <vm_name>
-gcloud compute scp --recurse ~/awni_speech/data/common-voice/ dzubke@$1:~/awni_speech/data/ --zone us-central1-c
-gcloud compute scp --recurse ~/awni_speech/data/voxforge/ dzubke@$1:~/awni_speech/data/ --zone us-central1-c
-gcloud compute scp --recurse ~/awni_speech/data/tatoeba/ dzubke@$1:~/awni_speech/data/ --zone us-central1-c
